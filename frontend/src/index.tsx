43687a90f37e288e1b17d1ee305a1d81b2147e44
Jed <jedster1111@hotmail.co.uk>
2018-11-05 00:42:01 +0000

    Created a typescript hooks example

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/examples/App";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-if (module.hot) {
-  module.hot.accept();
-}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
